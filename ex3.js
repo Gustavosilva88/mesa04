7d07a32cd44ce14844799dcd6cc4577cb6c6b70b
Gustavosilva88 <gustavo.vinicius88@gmail.com>
2022-08-09 22:27:51 -0300

    adicionando primeiro commit

---
+++ ex3.js
@@ -0,0 +1,11 @@
+const nome = "Aline"; //Constant 
+const sobrenome = "Ferreira"; //Constant 
+let idade = 33; // Variavel
+let peso = 67.3; // Variavel
+let altura = 1.53; // Variavel
+let plano = false; // 
+let imc = null;
+
+imc = peso / Math.pow(altura, 2); // Variavel
+
+console.log(nome + " " + sobrenome + " Tem " + idade + " anos e seu indice de massa corporal é de " + imc);

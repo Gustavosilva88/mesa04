0b675068bb6246e70468872f75ee0d93c4a4f4f1
Gustavosilva88 <gustavo.vinicius88@gmail.com>
2022-08-09 23:14:01 -0300

    adicionando ex2.js

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let nome = "Gustavo Vinicius da Silva"
+let idade = 34
+let usuarioAtivo = true
+
+console.log("Meu Nome: ")
+console.log(nome)
+console.log("Minha Idade: ")
+console.log(idade)
+console.log("Situação atual no sistema? ")
+console.log(usuarioAtivo)
